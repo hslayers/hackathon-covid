4222986903c06130652d0e83b23c0b1d1cd60dd2
SLeitgeb <leitgeb.simon@gmail.com>
2021-01-27 19:22:24 +0100

    Change basemap provider, add zoom buttons

--- ch12/src/app.js
+++ ch12/src/app.js
@@ -8,7 +8,7 @@ import 'query.module';
 import 'gallery.module';
 import 'angular-material';
 import { Vector, Tile } from 'ol/layer';
-import { OSM, XYZ, Vector as VectorSource } from 'ol/source';
+import { XYZ, Vector as VectorSource } from 'ol/source';
 import { GeoJSON } from 'ol/format';
 import { Style, Icon, Text, Fill, Stroke } from 'ol/style';
 import { transform } from 'ol/proj';
@@ -102,6 +102,7 @@ module.value('HsConfig', {
 	panelWidths: {
 		default: '60%',
 	},
+	popupOffset: [0, -27],
 	mapControls: [
 		new Attribution(),
 	],
@@ -121,7 +122,10 @@ module.value('HsConfig', {
 	},
 	default_layers: [
 		new Tile({
-			source: new OSM(),
+			source: new XYZ({
+				attributions: ['Maps <a href="http://www.thunderforest.com">Thunderforest</a>', 'Data <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'],
+				url: 'https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=820bd076452548f894e0059944475cb9',
+			}),
 			title: 'Base layer',
 			base: true,
 			visible: true,
